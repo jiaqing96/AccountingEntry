1ba5ff358a9d045a0c9df40f319fe6cae3238823
nana <jiaqing.liu@accenture.com>
2021-01-12 15:08:25 +0800

    AccountingEntry

--- force-app/main/default/aura/AccountingEntryBookController.js
+++ force-app/main/default/aura/AccountingEntryBookController.js
@@ -1,5 +1,10 @@
+ /*
+*  Name : Init
+*  Type : function
+*  Description : For foreground processing of AccountingEntryLighting component, see the helperMethod functions for details.
+*/
 ({
-	Init : function(component, event, helper) {
-        helper.helperMethod(component, "c.getAccountingEntry", true);
+	Init : function ( component, event, helper ) {
+        helper.helperMethod ( component, "c.getAccountingEntry", true );
     }
 })
--- force-app/main/default/aura/AccountingEntryBookHelper.js
+++ force-app/main/default/aura/AccountingEntryBookHelper.js
@@ -1,26 +1,30 @@
+ /*
+*  Name : helperMethod
+*  Type : function
+*  Description : Call the Backstage apex function and connect to the foreground component.
+                 If the condition is met, the Apex database query statement will be displayed in the Component. 
+                 If the condition is not met, an error reminder will be given
+*/
 ({
-	helperMethod : function(component, event, helper) {
-        var action = component.get("c.getAccountingEntry"); //获取apex方法与cmp连接
-        var x = component.get ( "v.number" ); //将前台用户输入rownumber赋给x
-        //var error = component.get ( "v.edit" ); //将前台attribute edit赋给error,判断true/false
-        action.setParams ( { n:x } ); //将用户输入的number传给apex n
-        action.setCallback ( this, function(response ) { //回调函数
-        	var state = response.getState ( ); //获取状态
-            console.log(state + "=======");
-            if ( state === "SUCCESS" ) { //当状态是成功的时候
-                var count=response.getReturnValue ( ).length; //获取apex sql list的长度
-                console.log (count+'!!!!!!!!!!!!!!' );
-                if(count > 0){ //当我的list符合条件的时候
-                    component.set ( "v.edit","true" );  //对前台aura:if进行true的赋值
-                    component.set ( "v.AccountingEntryBook", response.getReturnValue ( ) );//返回结果
+	helperMethod : function ( component, event, helper ) {
+        var action = component.get ( "c.getAccountingEntry" ); 
+        var x = component.get ( "v.number" ); 
+        action.setParams ( { n:x } ); 
+        action.setCallback ( this, function ( response ) { 
+        	var state = response.getState ( ); 
+            if ( state === "SUCCESS" ) { 
+                var count = response.getReturnValue ( ).length; 
+                if ( count > 0 ) { 
+                    component.set ( "v.edit", "true" );  
+                    component.set ( "v.AccountingEntryBook", response.getReturnValue ( ) );
                 }
                 else{
-                    component.set ( "v.edit","false" );  //对前台aura:if进行false的赋值
+                    component.set ( "v.edit", "false" );  
                 }	
             }
             else {
-           		console.log ( "Failed with state: " + state );
-           }
+            	console.log ( "Failed with state:s" + state );
+            }
         });
          $A.enqueueAction ( action );
 	}       
